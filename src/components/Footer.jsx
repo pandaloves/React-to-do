8374e317d5062d7a56392dfb15c841a70c3f323c
meiyi <meiyinghu18@gmail.com>
2024-09-22 20:30:29 +0200

    new

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="h-10 w-full text-center text-xs font-light  text-neutral-600 dark:text-neutral-400 bg-neutral-200 p-3">
-      <span>@ By Meiying Hu</span>
-    </div>
-  );
-};
-
-export default Footer;

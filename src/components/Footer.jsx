f7990ca0f67e429153dc17d19a91e18edc9a1532
meiyi <meiyinghu18@gmail.com>
2024-09-23 08:57:25 +0200

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="w-full min-w-screen h-16 left-0 bottom-0 flex flex-row justify-center items-center dark:text-neutral-400 bg-neutral-200 p-3">
       <p className="my-5 mx-auto text-xs font-light text-neutral-600">
-        2004 By Meiying Hu
+        @ By Meiying Hu
       </p>
     </div>
   );
